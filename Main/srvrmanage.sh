39a57aecc04a55d2d92bbb5a35a794a745f45013
CaptainDuck <jmdimayuga021@gmail.com>
2016-06-19 21:48:35 +0800

    Update srvrmanage.sh

--- Main/srvrmanage.sh
+++ Main/srvrmanage.sh
@@ -18,4 +18,11 @@ BLINK="$(tput blink)"      # make blink
 REVERSE="$(tput rev)"        # reverse
 
 echo "$BOLD $UNDERLINE PocketMine Manager Server Script = Running! $NORMAL"
-echo "$YELLOW Hello %USER! What would you like to do?"
+echo "$YELLOW Hello $USER! What would you like to do? $NORMAL"
+echo "1) Install an MCPE Server Installer.."
+echo "2) Install Different MCPE Server Plugins.."
+echo "3) Developer Information.."
+echo "4) Exit.."
+echo "If you have any problems, suggestions, or bugs.. Visit the GitHub site! https://github.com/CaptainDuck/MCPE-Server-Manager/issues.. :D"
+echo "Please answer by numbers before the choices :D."
+read choices;
